2b29ebb294335f3a263ec42476cd0400e69f82ad
Linard Jeremy <linard.jeremy.5@gmail.com>
2021-08-10 14:39:24 +0200

    création du projet et des premiers component

---
+++ blog/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+  const likes = 50;
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+       <Home />
+        <p>Liked { likes } times</p>
+        <p>{ 10 }</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
